5a0d27f29b8ed4a519d582e11558bc4bbfbe40c1
Roman Liukevich <r.liukevich@gmail.com>
2022-09-18 22:06:17 +0200

    Initial Commit

---
+++ NattyOrNot/NattyOrNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  NattyOrNotApp.swift
+//  NattyOrNot
+//
+//  Created by Roman Liukevich on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct NattyOrNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
